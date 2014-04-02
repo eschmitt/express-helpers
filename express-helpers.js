340fabd13ee98f13a2b055067b979565c18036ce
Eric Schmitt <ericschmitt1@gmail.com>
2014-04-01 20:57:05 -0700

    Add redirect functions.

--- express-helpers.js
+++ express-helpers.js
@@ -4,8 +4,28 @@ var _ = require('preludejs');
 var render = function (res, template, data) {
       res.render(template, data);
     }.autoCurry()
+
+//+ redirect :: Response -> String -> IO
+  , redirect = function (res, url) {
+      res.redirect(url);
+    }
+
+//+ redirectOrSendError :: Response -> String -> String -> (Error -> IO)
+  , redirectOrSendError = function (res, url, msg) {
+      return function (err) {
+        var error_msg = msg || err.msg || err;
+        if (err) {
+          console.log(err);
+          res.send(500, error_msg);
+        } else {
+          res.redirect(url);
+        }
+      }
+    }
   ;
 
 module.exports = {
   render: render
+, redirect: redirect
+, redirectOrSendError: redirectOrSendError
 }
